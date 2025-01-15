d038cf16ddc78ddf3a23e725d8bc7df5ab2ae5c8
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-01-14 22:04:03 -0300

    feat: create actions

---
+++ src/action.js
@@ -0,0 +1,15 @@
+import * as actionTypes from "./actionTypes";
+
+export const addTask = (task) => {
+  return {
+    type: actionTypes.ADD_TASK,
+    payload: { task },
+  };
+};
+
+export const removeTask = (id) => {
+  return {
+    type: actionTypes.REMOVE_TASK,
+    payload: { id },
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import store from "./store";
+import { addTask, removeTask } from "./action";
 
+store.dispatch(addTask("Learn Redux"));
+console.log(store.getState());
+store.dispatch(removeTask(1));
 console.log(store.getState());
